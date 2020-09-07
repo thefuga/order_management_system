177631b5463a5cedf5a6c73679389aae0197d167
Erick Fuga <erick_fuga@hotmail.com>
2020-09-07 16:10:10 -0300

    Add order scopes

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,4 +5,9 @@ class Order < ApplicationRecord
 
   validates :control_number, numericality: { only_integer: true },
                              uniqueness: true
+
+  scope :pending,           -> { where(state: "pending") }
+  scope :in_progress,       -> { where(state: "in_progress") }
+  scope :completed,         -> { where(state: "completed") }
+  scope :by_control_number, ->(control_number) { find_by(control_number: control_number)}
 end
---
+++ test/models/order/scopes_test.rb
@@ -0,0 +1,32 @@
+require 'test_helper'
+
+class OrderScopesTest < ActiveSupport::TestCase
+  test "Pending state scope" do
+    pending_orders = Order.pending
+
+    assert pending_orders.one?
+    assert_equal orders(:pending), pending_orders.first
+  end
+
+  test "In progress order states" do
+    in_progress_orders = Order.in_progress
+
+    assert in_progress_orders.one?
+    assert_equal orders(:in_progress), in_progress_orders.first
+  end
+
+  test "Completed order states" do
+    completed_orders = Order.completed
+
+    assert completed_orders.one?
+    assert_equal orders(:completed), completed_orders.first
+  end
+
+  test "By Control number scope" do
+    expected_order = orders(:pending)
+
+    found_order = Order.by_control_number(expected_order.control_number)
+
+    assert_equal expected_order, found_order
+  end
+end
